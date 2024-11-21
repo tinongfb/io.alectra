649b478e17ff33f57c19fd51c6fa74811dfe4c4f
Christine L. Ong <cong@sviisca.com>
2024-11-21 13:03:33 +0800

    new webapp created and to finish later

---
+++ webapps/my-webapp/public/script.js
@@ -0,0 +1,8 @@
+// Example: Add an event listener to a button
+document.getElementById("myButton").addEventListener("click", function () {
+    alert("Button clicked!");
+    console.log("Button was clicked!");
+  });
+  
+
+  
